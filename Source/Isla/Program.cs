89bc8d67286ed810ef185f9157bc87ac26f12517
Niall <50669114+niallVR@users.noreply.github.com>
2022-08-05 13:00:30 +0100

    Project: Bumped Launcher from 2.0.0 to 3.0.0

--- Source/Isla/Program.cs
+++ Source/Isla/Program.cs
@@ -5,28 +5,22 @@ using Isla.Database.Installers;
 using Isla.Modules.Activity.Installers;
 using Isla.Modules.Notifications.Installers;
 using Isla.Modules.Roles.Installers;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NiallVR.Launcher.Configuration.Binding.Extensions;
+using NiallVR.Launcher.Configuration.Setup.Extensions;
 using NiallVR.Launcher.Configuration.Validation.Extensions;
 using NiallVR.Launcher.Logging.Extensions;
 using Serilog;
 using DiscordConfig = Isla.Config.DiscordConfig;
 
 await Host.CreateDefaultBuilder()
-    .ConfigureSerilog((services, config) =>
+    .UseSerilog((_, services, config) =>
     {
-        config.ReadFrom.Configuration(services.GetRequiredService<IConfiguration>());
-        config.SetupConsoleSink();
+        config.UseConsoleSink();
+        config.UseConfiguration(services);
     })
-    .ConfigureAppConfiguration(builder =>
-    {
-        builder.AddYamlFile("appsettings.yaml", optional: false);
-        builder.AddYamlFile("appsettings.Development.yaml", optional: true);
-        builder.AddYamlFile("appsettings.Production.yaml", optional: true);
-    })
-    .AddConfigValidation(services =>
+    .UseYamlConfig()
+    .UseConfigValidation(services =>
     {
         services.BindConfig<ActivityConfig>("Activity");
         services.BindConfig<DatabaseConfig>("Database");
@@ -49,9 +43,9 @@ await Host.CreateDefaultBuilder()
         {
             settings.Token = s.GetRequiredService<DiscordConfig>().Token!;
             settings.DiscordConfig.AlwaysDownloadUsers = true;
-            settings.DiscordConfig.GatewayIntents = 
-                GatewayIntents.Guilds | 
-                GatewayIntents.GuildMembers | 
+            settings.DiscordConfig.GatewayIntents =
+                GatewayIntents.Guilds |
+                GatewayIntents.GuildMembers |
                 GatewayIntents.GuildScheduledEvents;
         });
     })
